c4b5224b606d4847b62e38099ce84d77fd13abf3
Sebastian <sebastian.rapa16@gmail.com>
2020-10-03 10:05:38 +0300

    ATMMachineApplication class is marked with @SpringBootApplication this way spring knows to start the application using the main method inside this class;

---
+++ src/main/java/ro/sebastianrapa/atmapp/ATMMachineApplication.java
@@ -0,0 +1,12 @@
+package ro.sebastianrapa.atmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATMMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ATMMachineApplication.class, args);
+    }
+}
